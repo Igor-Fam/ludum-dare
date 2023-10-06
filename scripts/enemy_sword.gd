96ef194fd915693c628574b1054fdd1e3e35e4e0
Igor-Fam <igor.mfam@gmail.com>
2023-10-06 14:05:04 -0300

    Fixed sword hitbox and last level not working as intended

--- scripts/enemy_sword.gd
+++ scripts/enemy_sword.gd
@@ -3,7 +3,7 @@ class_name EnemySword
 
 var direction
 var cooldown = 0
-
+var swing_time = 0.23
 
 
 @onready var hitbox = $Hitbox
@@ -21,13 +21,17 @@ func _physics_process(delta):
 	collision.disabled = true
 	
 	if(sprite.animation == "swing"):
-		if(sprite.frame >= 2):
+		if(swing_time <= 0.17):
 			collision.disabled = false
 		
 		sprite.position.x = 13 * direction
-		if sprite.frame_progress == 1:
+		
+		if swing_time <=0:
 			sprite.animation = "idle"
 			sprite.position.x = 6 * direction
+			swing_time = 0.23
+		else:
+			swing_time -= delta
 	else:
 		sprite.position.x = 6 * direction
 
--- scripts/sword.gd
+++ scripts/sword.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Sword
 
 var direction
+var swing_time = 0.23
 
 @onready var collision = $CollisionShape2D
 @onready var sprite = $Sprite2D
@@ -15,18 +16,23 @@ func _physics_process(delta):
 	collision.disabled = true
 	
 	if(sprite.animation == "swing"):
-		if(sprite.frame >= 2):
+		if(swing_time <= 0.17):
 			collision.disabled = false
 		
 		sprite.position.x = 13 * direction
-		if sprite.frame_progress == 1:
+		
+		if swing_time <=0:
 			sprite.animation = "idle"
 			sprite.position.x = 6 * direction
+			swing_time = 0.23
+		else:
+			swing_time -= delta
 	else:
 		sprite.position.x = 6 * direction
 
 	
 	if(Input.is_action_just_pressed("ui_right_click")):
+		sprite.animation = "swing"
 		sprite.play("swing")
 		SoundPlayer.play(SoundPlayer.SWORD)
 		
